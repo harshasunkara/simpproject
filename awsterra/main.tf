c181bb1cd4979fb33910912383648eade5db9306
Harshavardhan <102301866+harshasunkara@users.noreply.github.com>
2022-05-03 09:28:49 +0530

    Delete main.tf

--- awsterra/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "example" {
-    ami = data.aws_ami.windows.id
-    instance_type = "t2.micro"
-    availability_zone = var.availability_zone
-  
-  lifecycle {
-    ignore_changes = [ami]
-  }
-}
